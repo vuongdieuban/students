d39beb1fbe8ddfa85cc9650a3365e85a4e43c1bb
Ban Vuong <vuongdieuban@gmail.com>
2019-10-12 19:54:20 -0600

    add .env

--- src/services/studentsService.js
+++ src/services/studentsService.js
@@ -1,4 +1,5 @@
-const URL = "https://www.hatchways.io/api/assessment/students";
+const URL = process.env.REACT_APP_API_URL;
+console.log(URL);
 
 export async function getStudents() {
   const data = await fetch(URL);
