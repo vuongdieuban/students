3a5682bb9c7824a89318345b10962811f2bc7cd0
Ban Vuong <vuongdieuban@gmail.com>
2019-10-20 16:40:33 -0600

    prod

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bccb42f0a67f0915387f1085601e969a.js"
+  "/precache-manifest.393b174c2c4ca4650799331c23988779.js"
 );
 
 self.addEventListener('message', (event) => {
--- frontend/src/services/studentsService.js
+++ frontend/src/services/studentsService.js
@@ -1,9 +1,8 @@
 // const URL = process.env.REACT_APP_API_URL;
-const URL = "http://api.students.banvuong.com";
-console.log("Url", URL);
+// const URL = "http://api.students.banvuong.com";
+const URL = "https://www.hatchways.io/api/assessment/students";
 export async function getStudents() {
   const data = await fetch(URL);
   const { students } = await data.json();
-  console.log("students", students);
   return students;
 }
