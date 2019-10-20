832f1625c16869dacb1190ed09a11f666a55a27a
Ban Vuong <vuongdieuban@gmail.com>
2019-10-20 13:09:10 -0600

    add async to db connect function

--- backend/startup/db.js
+++ backend/startup/db.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 
-module.exports = function() {
+module.exports = async function() {
   // Connect to Mongodb
-  try{
-    await mongoose.connect("mongodb://localhost/students",{
-      useNewUrlParser:true,
-      useUnifiedTopology:true,
-      useFindAndModify:false
-    })
-    console.log("Success! Connected to DB")
-  }catch(err){
+  try {
+    await mongoose.connect("mongodb://localhost/students", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+    console.log("Success! Connected to DB");
+  } catch (err) {
     console.log("Fail to connect to DB\n", err.message);
-    process.exit(1)
+    process.exit(1);
   }
 };
