cfd57e86d6d005945a6eadd4ba2fe147195fbf52
Ban Vuong <vuongdieuban@gmail.com>
2019-10-20 14:14:31 -0600

    GET and POST student

--- backend/routes/students.js
+++ backend/routes/students.js
@@ -1,8 +1,32 @@
+const config = require("config");
 const express = require("express");
 const router = express.Router();
+const { Student, validate } = require("../models/students");
+const BASE_URL = config.get("BaseUrl");
+const IMG_PATH = "student_img.jpg";
 
 router.get("/", async (req, res) => {
-  res.json("Hello world");
+  const students = await Student.find();
+  if (!students) return res.status(404).json("Cannot find any student");
+  res.json(students);
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).json(error.details[0].message);
+  const { firstName, lastName, email, company, city, skill, grades } = req.body;
+  const newStudent = new Student({
+    firstName,
+    lastName,
+    email,
+    company,
+    city,
+    skill,
+    grades
+  });
+  newStudent.pic = BASE_URL + "/" + IMG_PATH;
+  await newStudent.save();
+  res.json(newStudent);
 });
 
 module.exports = router;
--- backend/startup/routes.js
+++ backend/startup/routes.js
@@ -6,5 +6,8 @@ module.exports = function(app) {
   app.use(express.json());
   app.use(cors());
 
+  // allow to access static folder
+  app.use("/", express.static("static"));
+
   app.use("/", students);
 };
